ae333057e9183807b692961e9b485051d8f65712
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-06-21 18:08:36 -0300

    Debo implementar las listas enlaza
    Debo implementar las listas enlaza

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -35,33 +35,47 @@ class Bibloteca:
             self.usuarios[email_usuario] = nuevo_usuario
             print(f'Usuario "{nombre_usuario}" registrado con éxito.')
 
-"""
-    def prestar_libro(self, usuario_id, libro):
-        if usuario_id not in self.usuarios:
-            print(f'El usuario con ID {usuario_id} no está registrado.')
+
+    def prestar_libro(self, email_usuario, nombre_usuario, libro):
+        if email_usuario not in self.usuarios:
+            print(f'El usuario con email {email_usuario} no está registrado.')
             return
         if libro not in self.libros_disponibles:
             print(f'El libro "{libro}" no está disponible.')
             return
         self.libros_disponibles.remove(libro)
-        self.prestamos[libro] = usuario_id
-        print(f'Libro "{libro}" prestado a usuario "{self.usuarios[usuario_id]}".')
+        self.prestamos[libro] = email_usuario
+        print(f'Libro "{libro}" prestado a "{nombre_usuario}".')
 
-    def aceptar_devolucion(self, libro):
+    def aceptar_devolucion(self, email_usuario, nombre_usuario, libro):
         if libro not in self.prestamos:
             print(f'El libro "{libro}" no está prestado.')
             return
-        usuario_id = self.prestamos.pop(libro)
+        email_usuario = self.prestamos.pop(libro)
         self.libros_disponibles.append(libro)
-        print(f'Libro "{libro}" devuelto por usuario "{self.usuarios[usuario_id]}".')
-"""
+        print(f'Libro "{libro}" devuelto por usuario "{nombre_usuario}".')
 
-# Ejemplo de uso
+
+# BANCO DE PRUEBAS
+
 bibloteca = Bibloteca('Bibloteca Central', 'Calle Falsa 123', '123-4567', 'info@biblioteca.com', 'Lun-Vie 9:00-18:00')
 libro1 = Libro ("El señor de los anillos", "D&D", "1969", "Una guerra épica por el Anillo del Poder")
 
 bibloteca.agregar_libro("El señor de los anillos", "D&D", "1969", "Pelar por el Anillo del Poder")
-bibloteca.agregar_libro("Teoria de La Estetica del Arte", "Alianza", "1969", "Libro de la Estetica del Arte")
+
 
 bibloteca.ingresar_usuario("Matias Alessandrello", "Av.Libertad 1234", 1536690663, "mati@correo.com.ar", "31/01/1967")
+
+bibloteca.prestar_libro("mati@correo.com.ar", "Matias Alessandrello", "El señor de los anillos")
+bibloteca.prestar_libro("dani@correo.com.ar", "Daniel Leso", "Teoria de La Estetica del Arte")
+
+bibloteca.ingresar_usuario("Daniel Leso", "Av.Corrientes 1234", 1536690754, "dani@correo.com.ar", "10/05/1988")
+bibloteca.prestar_libro("dani@correo.com.ar", "Daniel Leso", "Teoria de La Estetica del Arte")
+
+bibloteca.agregar_libro("Teoria de La Estetica del Arte", "Alianza", "1969", "Libro de T.H Adorno sobre de la Estetica del Arte")
+bibloteca.prestar_libro("dani@correo.com.ar", "Daniel Leso", "Teoria de La Estetica del Arte")
+bibloteca.prestar_libro("dani@correo.com.ar", "Daniel Leso", "El señor de los anillos")
+
+bibloteca.aceptar_devolucion("mati@correo.com.ar", "Matias Alessandrello", "El señor de los anillos" )
+bibloteca.prestar_libro("dani@correo.com.ar", "Daniel Leso", "El señor de los anillos")
 
